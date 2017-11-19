8d526e95187f41162609f3e7f1e0f52dce4b3fe4
Thomas Juhasz <juhasz.thomas.jozsef@gmail.com>
2017-11-19 09:00:59 +0100

    new chart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DiagramComponent } from './diagram/diagram.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
-  declarations: [AppComponent, DiagramComponent],
+  declarations: [AppComponent, DiagramComponent, ChartComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
